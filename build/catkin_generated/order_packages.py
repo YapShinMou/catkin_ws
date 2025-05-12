db42c83e6605d1f4e811095665e3a56c4ecbfb0a
YapShinMou <arthurxiyap@gmail.com>
2025-05-11 23:45:45 -0700

    topic_cpp_note_3

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = '/home/yap/catkin_ws/src'
 whitelisted_packages = ''.split(';') if '' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/yap/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/yap/catkin_ws/devel;/opt/ros/noetic' != '' else []
+underlay_workspaces = '/home/yap/githubtest/devel;/home/yap/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/yap/githubtest/devel;/home/yap/catkin_ws/devel;/opt/ros/noetic' != '' else []
